c96136202537ddce8a365c237608d2b2ed0cae87
Joseph Hellerstein <jlheller@uw.edu>
2015-09-03 18:19:37 -0700

    Working on tbl context menu

--- yui/MergedDataTable/custom.js
+++ yui/MergedDataTable/custom.js
@@ -14,22 +14,34 @@ $(document).click(function (event) {
   }
 });
 
-var aMenuItems = ["Edit Name", "Clone", "Delete" ];
+var colMenuItems = ["Rename", "Delete" ];
+var tblMenuItems = ["Rename", "Delete", "Add-Col" ];
 
 /*
   Instantiate a ContextMenu:  The first argument passed to the constructor
   is the id for the Menu element to be created, the second is an 
   object literal of configuration properties.
  */
-var oEweContextMenu = new YAHOO.widget.ContextMenu("trymenu",
+// BUG: Not getting the correct DOM element. Consider putting trigger on the table and then not bubbling for column?
+var el = $( "caption" );
+var tblContextMenu = new YAHOO.widget.ContextMenu("tab-menu",
       {
-      trigger: ["yui-dt8-th-address", "yui-dt8-th-city"],
-      itemdata: aMenuItems,
+      trigger: [ el ],  // Templatetize dt{#columns} & iterate across column names
+      itemdata: tblMenuItems,
       lazyload: true
     }
     );
 
-function onEweContextMenuClick(p_sType, p_aArgs) {
+var colContextMenu = new YAHOO.widget.ContextMenu("col-menu",
+      {
+      trigger: ["yui-dt3-th-address", "yui-dt3-th-salary"],  // Templatetize dt{#columns} & iterate across column names
+      itemdata: colMenuItems,
+      lazyload: true
+    }
+    );
+
+
+function onTblContextMenuClick(p_sType, p_aArgs) {
 /*
   The second item in the arguments array (p_aArgs) 
   passed back to the "click" event handler is the 
@@ -39,28 +51,48 @@ function onEweContextMenuClick(p_sType, p_aArgs) {
   "use strict";
   var oItem = p_aArgs[1], // The MenuItem that was clicked
     oTarget = this.contextEventTarget;
-  alert("Invoked context menu");
+  alert("Invoked table context menu");
+  return oItem + oTarget; /* dummy */
+}
+
+function onColContextMenuClick(p_sType, p_aArgs) {
+/*
+  The second item in the arguments array (p_aArgs) 
+  passed back to the "click" event handler is the 
+  MenuItem instance that was the target of the 
+  "click" event.
+*/
+  "use strict";
+  var oItem = p_aArgs[1], // The MenuItem that was clicked
+    oTarget = this.contextEventTarget;
+  alert("Invoked column context menu");
   return oItem + oTarget; /* dummy */
 }
 
 // "render" event handler for the ewe context menu
 //  Add a "click" event handler to the ewe context menu
-function onContextMenuRender(p_sType, p_aArgs) {
+function onColContextMenuRender(p_sType, p_aArgs) {
   "use strict";
-  this.subscribe("click", onEweContextMenuClick);
+  this.subscribe("click", onColContextMenuClick);
+}
+
+function onTblContextMenuRender(p_sType, p_aArgs) {
+  "use strict";
+  this.subscribe("click", onTblContextMenuClick);
 }
 
 // "click" event handler for each item in the ewe context menu
 // Add a "render" event handler to the ewe context menu
-oEweContextMenu.subscribe("render", onContextMenuRender);
+tblContextMenu.subscribe("render", onTblContextMenuRender);
+colContextMenu.subscribe("render", onColContextMenuRender);
 
 
 var newDataSource = [
-    {name: "John A. Smith", address: "1236 Some Street", city: "San Francisco", state: "CA", amount: 5, active: "yes", colors: ["red"], last_login: "4/19/2007"},
-    {name: "Joan B. Jones", address: "3271 Another Ave", city: "New York", state: "NY", amount: 3, active: "no", colors: ["red", "blue"], last_login: "2/15/2006"},
-    {name: "Bob C. Uncle", address: "9996 Random Road", city: "Los Angeles", state: "CA", amount: 0, active: "maybe", colors: ["green"], last_login: "1/23/2004"},
-    {name: "John D. Smith", address: "1623 Some Street", city: "San Francisco", state: "CA", amount: 5, active: "yes", colors: ["red"], last_login: "4/19/2007"},
-    {name: "Joan E. Jones", address: "3217 Another Ave", city: "New York", state: "NY", amount: 3, active: "no", colors: ["red", "blue"], last_login: "2/15/2006"}];
+    {name: "John A. Smith", address: "1236 Some Street", salary: "12.33"},
+    {name: "Joan B. Jones", address: "3271 Another Ave", salary: "34556"},
+    {name: "Bob C. Uncle", address: "9996 Random Road", salary: "893"},
+    {name: "John D. Smith", address: "1623 Some Street", salary: "0.092"},
+    {name: "Joan E. Jones", address: "3217 Another Ave", salary: "23456"}];
 
 YAHOO.util.Event.addListener(window, "load", function () {
   // Custom formatter for "address" column to preserve line breaks
@@ -70,24 +102,22 @@ YAHOO.util.Event.addListener(window, "load", function () {
         elCell.innerHTML = "<pre class=\"address\">" + YAHOO.lang.escapeHTML(oData) + "</pre>";
       },
       myColumnDefs = [
-        {key: "uneditable"},
+        {key: "name", formatter: formatAddress, editor:  new YAHOO.widget.TextareaCellEditor()},
         {key: "address", formatter: formatAddress, editor:  new YAHOO.widget.TextareaCellEditor()},
-        {key: "city", formatter: "text", editor:  new YAHOO.widget.TextboxCellEditor({disableBtns: true})},
-        {key: "state", editor:  new YAHOO.widget.DropdownCellEditor({dropdownOptions: YAHOO.example.Data.stateAbbrs, disableBtns: true})},
-        {key: "amount", editor:  new YAHOO.widget.TextboxCellEditor({validator: YAHOO.widget.DataTable.validateNumber})},
-        {key: "active", editor:  new YAHOO.widget.RadioCellEditor({radioOptions: ["yes", "no", "maybe"], disableBtns: true})},
-        {key: "colors", editor:  new YAHOO.widget.CheckboxCellEditor({checkboxOptions: ["red", "yellow", "blue"]})},
-        {key: "fruit", editor:  new YAHOO.widget.DropdownCellEditor({multiple: true, dropdownOptions: ["apple", "banana", "cherry"]})},
-        {key: "last_login", formatter: YAHOO.widget.DataTable.formatDate, editor:  new YAHOO.widget.DateCellEditor()}
+        {key: "salary", formatter: formatAddress, editor:  new YAHOO.widget.TextareaCellEditor()}
       ],
       myDataSource = new YAHOO.util.DataSource(newDataSource);
     myDataSource.responseType = YAHOO.util.DataSource.TYPE_JSARRAY;
     myDataSource.responseSchema = {
-      fields: ["address", "city", "state", "amount", "active", "colors", "fruit", {key: "last_login", parser: "date"}]
+      fields: ["name", "address", "salary"]
     };
-  
-    var myDataTable = new YAHOO.widget.DataTable("cellediting", myColumnDefs, myDataSource, {});
-  
+
+    var myDataTable = new YAHOO.widget.DataTable("cellediting", myColumnDefs, myDataSource,
+        {
+          caption: "New table"
+        }
+        );
+
     // Set up editing flow
     var highlightEditableCell = function (oArgs) {
       var elCell = oArgs.target;
