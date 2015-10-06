a19f9d43d8764d78bb9d1cc0d33f6fe35a996ef0
Joseph Hellerstein <jlheller@uw.edu>
2015-10-05 20:31:11 -0700

    Using static files correctly

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -87,7 +87,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "mysite/static"),
     )
 
 # Heatmap constants
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from mysite import settings
 
 urlpatterns = patterns('',
     # Examples:
