165c615fd8df48492efd669633686b942b7b950e
Joseph Hellerstein <jlheller@uw.edu>
2015-10-08 13:51:07 -0700

    Towards using templates for data values.

--- mysite/scisheets/helpers/scisheets_views.py
+++ mysite/scisheets/helpers/scisheets_views.py
@@ -14,15 +14,24 @@ import os
 # This version uses templates to generate column names
 def scisheets(request):
   column_names = ["row", "name", "address", "salary"]
-  initial_column_names = column_names[0:-1]
   final_column_name = column_names[-1]
   table_id = "cellediting"
-  table_caption = "New table"
+  table_caption = "Demo Table"
+  data = []
+  data.append(["1", "John A. Smith", "1236 Some Street", "12.33"])
+  data.append(["2", "Joan B. Jones", "3271 Another Ave", "34556"])
+  data.append(["3", "Bob C. Uncle", "9996 Random Road", "893"])
+  data_dic = {}
+  for r in range(len(data)):
+    for c in range(len(column_names)):
+      data_dic[column_names[c]] = data[r][c] 
   ctx_dict = {'column_names': column_names,
-              'initial_column_names': initial_column_names,
               'final_column_name': final_column_name,
               'table_id': table_id,
               'table_caption': table_caption,
+              'data_dic': data_dic,
+              'data': data,
+              'final_data': data[-1],
              }
   html = get_template('scitable.html').render(ctx_dict)
   return HttpResponse(html)
