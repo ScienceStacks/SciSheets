515aadc49d1714a14026e0ea17ba60dbce08da0c
Joseph Hellerstein <jlheller@uw.edu>
2015-10-08 12:50:12 -0700

    Towards putting display data in template

--- mysite/scisheets/helpers/scisheets_views.py
+++ mysite/scisheets/helpers/scisheets_views.py
@@ -11,6 +11,18 @@ import datetime
 import json
 import os
 
+# This version uses templates to generate column names
 def scisheets(request):
-  html = get_template('scitable.html').render({})
+  column_names = ["row", "name", "address", "salary"]
+  initial_column_names = column_names[0:-1]
+  final_column_name = column_names[-1]
+  table_id = "cellediting"
+  table_caption = "New table"
+  ctx_dict = {'column_names': column_names,
+              'initial_column_names': initial_column_names,
+              'final_column_name': final_column_name,
+              'table_id': table_id,
+              'table_caption': table_caption,
+             }
+  html = get_template('scitable.html').render(ctx_dict)
   return HttpResponse(html)
