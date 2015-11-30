1992243d15e63dcc124e09abd06c1c99c8eb7995
Joseph Hellerstein <jlheller@uw.edu>
2015-11-30 14:12:42 -0800

    Completed tests for column.

--- mysite/scisheets/core/test_column.py
+++ mysite/scisheets/core/test_column.py
@@ -18,6 +18,10 @@ FORMULA = "A+B"
 #############################
 class TestColumn(unittest.TestCase):
 
+  def setUp(self):
+    self.column = createColumn(COLUMN_NAME, data=LIST, table=None,
+        formula=FORMULA)
+
   def testConstructor(self):
     column = cl.Column(COLUMN_NAME)
     self.assertEqual(column._name, COLUMN_NAME)
@@ -39,64 +43,51 @@ class TestColumn(unittest.TestCase):
     self.assertTrue(compareValues(column._data_values, ARRAY))
 
   def testCopy(self):
-    column = createColumn(COLUMN_NAME, data=LIST, table=TABLE,
-        formula=FORMULA)
-    column_copy = column.copy()
-    self.assertEqual(column._name, column_copy._name)
-    self.assertTrue(compareValues(column._data_values, column_copy._data_values))
-    self.assertEqual(column._formula, column_copy._formula)
+    column_copy = self.column.copy()
+    self.assertEqual(self.column._name, column_copy._name)
+    self.assertTrue(compareValues(self.column._data_values, 
+        column_copy._data_values))
+    self.assertEqual(self.column._formula, column_copy._formula)
     self.assertIsNone(column_copy._owning_table)
 
   def testDeleteCells(self):
-    column = createColumn(COLUMN_NAME, data=LIST, table=TABLE,
-        formula=FORMULA)
     INDEX = 0
     NON_INDEX = 1
-    column.deleteCells([INDEX])
-    self.assertEqual(column._data_values[INDEX], LIST[NON_INDEX])
+    self.column.deleteCells([INDEX])
+    self.assertEqual(self.column._data_values[INDEX], LIST[NON_INDEX])
 
   def testEvaluate(self):
-    column = createColumn(COLUMN_NAME, data=LIST, table=TABLE,
-        formula=FORMULA)
-    self.assertRaises(er.NotYetImplemented, column.evaluate)
+    self.assertRaises(er.NotYetImplemented, self.column.evaluate)
 
   def testGetCells(self):
-    column = createColumn(COLUMN_NAME, data=LIST, table=TABLE,
-        formula=FORMULA)
-    cells = column.getCells()
-    self.assertTrue(compareValues(column._data_values, cells))
+    cells = self.column.getCells()
+    self.assertTrue(compareValues(self.column._data_values, cells))
 
   def testNumCells(self):
-    column = createColumn(COLUMN_NAME, data=LIST, table=TABLE,
-        formula=FORMULA)
-    self.assertEqual(column.numCells(), len(LIST))
+    self.assertEqual(self.column.numCells(), len(LIST))
 
   def testGetName(self):
-    column = createColumn(COLUMN_NAME, data=LIST, table=TABLE,
-        formula=FORMULA)
-    self.assertEqual(column.getName(), column._name)
+    self.assertEqual(self.column.getName(), COLUMN_NAME)
 
   def testSetFormula(self):
-    column = createColumn(COLUMN_NAME, data=LIST, table=TABLE,
-        formula=None)
-    column.setFormula(FORMULA)
-    self.assertEqual(column._formula, FORMULA)
+    self.column.setFormula(FORMULA)
+    self.assertEqual(self.column._formula, FORMULA)
 
   def testSetTable(self):
-    column = createColumn(COLUMN_NAME, data=LIST, table=None,
-        formula=FORMULA)
-    column.setTable(TABLE)
-    self.assertEqual(column._owning_table, TABLE)
+    self.column.setTable(TABLE)
+    self.assertEqual(self.column._owning_table, TABLE)
 
   def testUpdateCell(self):
-    column = createColumn(COLUMN_NAME, data=LIST, table=None,
-        formula=FORMULA)
     INDEX = 0
     new_value = LIST[INDEX] + 10
-    column.updateCell(new_value, INDEX)
-    self.assertEqual(column._data_values[INDEX], new_value)
-    
-    
+    self.column.updateCell(new_value, INDEX)
+    self.assertEqual(self.column._data_values[INDEX], new_value)
+
+  def testInsertCell(self):
+    NEW_VALUE = 30
+    self.column.insertCell(NEW_VALUE)
+    index = len(LIST)
+    self.assertEqual(self.column.getCells()[index], NEW_VALUE)
 
 
 if __name__ == '__main__':
