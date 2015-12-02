5a3336e72e17a0a234e90a3132564f53f5f00d9c
Joseph Hellerstein <jlheller@uw.edu>
2015-12-01 16:49:51 -0800

    Completed tests for scisheets_views

--- mysite/scisheets/helpers/test_scisheets_views.py
+++ mysite/scisheets/helpers/test_scisheets_views.py
@@ -9,47 +9,43 @@ import scisheets_views as sv
 import os
 
 
+NCOL = 3
+NROW = 4
+BASE_URL = "http://localhost:8000/scisheets/"
+TABLE_PARAMS = [NCOL, NROW]
+
+
 class TestScisheetsViews(TestCase):
  
   def setUp(self):
     self.factory = RequestFactory()
-
-  def _createRequestWithCommand(self):
-    TARGET = 'Cell'
-    COMMAND = 'Update'
-    VALUE = 'XXX'
-    ROW_INDEX = 1
-    COLUMN_INDEX = 3
-    TABLE_NAME = 'XYZ'
-    cmd_dict = self._commandFactory()
-    cmd_dict['target'] = TARGET
-    cmd_dict['command'] = COMMAND
-    cmd_dict['value'] = VALUE
-    cmd_dict['row_index'] = ROW_INDEX
-    cmd_dict['column_index'] = COLUMN_INDEX
-    cmd_dict['table_name'] = TABLE_NAME
-    request = self._createRequestFromCommandDict(cmd_dict)
-    return request
 
   def _addSessionToRequest(self, request):
     middleware = SessionMiddleware()
     middleware.process_request(request)
     request.session.save()
 
-  def _commandFactory(self):
+  def _commandDictFactory(self):
+    TARGET = 'Cell'
+    COMMAND = 'Update'
+    VALUE = 'XXX'
+    ROW_INDEX = 1
+    COLUMN_INDEX = 3
+    TABLE_NAME = 'XYZ'
     cmd_dict = {}
-    cmd_dict['command'] = None
-    cmd_dict['target'] = None
-    cmd_dict['table_name'] = None
-    cmd_dict['column_index'] = None
-    cmd_dict['row_index'] = None
-    cmd_dict['value'] = None
+    cmd_dict['target'] = TARGET
+    cmd_dict['command'] = COMMAND
+    cmd_dict['value'] = VALUE
+    cmd_dict['row_index'] = ROW_INDEX
+    cmd_dict['column_index'] = COLUMN_INDEX
+    cmd_dict['table_name'] = TABLE_NAME
     return cmd_dict
 
   def _createURL(self, address="dummy", count=None, values=None, names=None):
     # Input: count - number of variables
     #        names - variable names
     #        values - values to use for each variable
+    #        address - URL address
     # Returns - a URL string
     url = address
     if values is not None:
@@ -81,15 +77,14 @@ class TestScisheetsViews(TestCase):
     # Returns - request with count number of parameters
     return self.factory.get(url)
 
-  def _createRequestFromCommandDict(self, cmd_dict):
+  def _createURLFromCommandDict(self, cmd_dict, address=None):
     # Input: cmd_dict - command dictionary from commandFactory
-    # Output: HTML request with session variable
+    # Output: URL
     names = cmd_dict.keys()
     values = []
     for k in names:
       values.append(cmd_dict[k])
-    request = self._URL2Request(self._createURL(values=values, names=names))
-    self._addSessionToRequest(request)
+    return self._createURL(values=values, names=names, address=address)
 
   def testCreateCommandDict(rquest):
     cmd_dict = sv.createCommandDict(self.request)
@@ -128,16 +123,74 @@ class TestScisheetsViews(TestCase):
     new_table = sv.unPickleTable(request)
     self.assertEqual(new_table.getName(), table.getName())
 
-  def testScisheets(self):
-    NCOL = 3
-    NROW = 4
-    client_url = "http://localhost:8000/scisheets/%d/%d/" % (NCOL, NROW)
-    response = self.client.get(client_url)
+  def _createBaseURL(self, params=None):
+    # Creates the base URL to construct a table
+    # Input: params
+    #         0 - number of columns
+    #         1 - number of rows
+    # Output: URL
+    if params is None:
+      client_url = BASE_URL
+    else:
+      ncol = params[0]
+      nrow = params[1]
+      client_url = "%s%d/%d/" % (BASE_URL, ncol, nrow)
+    return client_url
+
+  def _createBaseTable(self):
+    # Create the table
+    create_table_url = self._createBaseURL(params=TABLE_PARAMS)
+    return self.client.get(create_table_url)
+
+  def _verifyResponse(self, response, checkSessionid=True):
     self.assertEqual(response.status_code, 200)
-    self.assertTrue(response.cookies.has_key('sessionid'))
+    if checkSessionid:
+      self.assertTrue(response.cookies.has_key('sessionid'))
     expected_keys = ['column_names', 'final_column_name', 
         'table_id', 'table_caption', 'data']
     self.assertTrue(response.context.keys().issuperset(expected_keys))
+
+  def testScisheets(self):
+    # Test creation of the initial random table
+    response = self._createBaseTable()
+    self._verifyResponse(response)
+
+  def testScisheetsCommandReload(self):
+    self._createBaseTable()
+    # Do the refresh
+    refresh_url = self._createBaseURL()
+    response = self.client.get(refresh_url)
+    self._verifyResponse(response, checkSessionid=False)
+
+  def testScisheetsCommandCellUpdate(self):
+    self._createBaseTable()
+    # Do the cell update
+    create_table_url = self._createBaseURL(params=TABLE_PARAMS)
+    ROW_INDEX =1
+    COLUMN_INDEX = 2
+    VALUE = 9999
+    cmd_dict = self._commandDictFactory()
+    cmd_dict['target'] = 'Cell'
+    cmd_dict['command'] = 'Update'
+    cmd_dict['row_index'] = ROW_INDEX
+    cmd_dict['column_index'] = COLUMN_INDEX
+    cmd_dict['value'] = VALUE
+    command_url = self._createURLFromCommandDict(cmd_dict, address=create_table_url)
+    response = self.client.get(command_url)
+    self._verifyResponse(response, checkSessionid=False)
+
+  def testScisheetsCommandColumnDelete(self):
+    self._createBaseTable()
+    # Do the cell update
+    create_table_url = self._createBaseURL(params=TABLE_PARAMS)
+    COLUMN_INDEX = 2
+    cmd_dict = self._commandDictFactory()
+    cmd_dict['target'] = 'Column'
+    cmd_dict['command'] = 'Delete'
+    cmd_dict['column_index'] = COLUMN_INDEX
+    command_url = self._createURLFromCommandDict(cmd_dict, address=create_table_url)
+    response = self.client.get(command_url)
+    self._verifyResponse(response, checkSessionid=False)
 
 
 if __name__ == '__main__':
