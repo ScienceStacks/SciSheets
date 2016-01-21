4e929c6b37849cf0f8247a78868e8819e9770d2a
Joseph Hellerstein <jlheller@uw.edu>
2016-01-21 10:58:16 -0800

    Refactored UI command processing.

--- mysite/scisheets/ui/ui_table.py
+++ mysite/scisheets/ui/ui_table.py
@@ -75,16 +75,15 @@ class UITable(Table):
     return table
 
   @staticmethod
-  def _createResponse(success=False, message="OK"):
+  def _createResponse(error):
     # Returns a response of the desired type
-    # Input: success - successful response if true,
-    #                  unsuccessful response if false
-    #        message - text sent back
+    # Input: error - result of processing a command
+    #                (may be None)
     # Output: response
-    if success:
-      response = {'data': message, 'success': True}
+    if error is None:
+      response = {'data': "OK", 'success': True}
     else:
-      response = {'data': message, 'success': False}
+      response = {'data': str(error), 'success': False}
     return response
   
 
@@ -100,66 +99,99 @@ class UITable(Table):
     # Output: response - Dictionary with response
     #            data: data returned
     #            success: True/False
-    response = self._createResponse(success=False)
-    # Table
-    # Cells
     target = cmd_dict["target"]
-    command = cmd_dict["command"]
     if target == "Cell":
-      if command == "Update":
-        self.updateCell(cmd_dict["value"], 
-                        cmd_dict["row_index"], 
-                        cmd_dict["column_index"])
-      else:
-        msg = "Unimplemented %s command: %s." % (target, command)
-        raise NotYetImplemented(msg)
+      error = self._cellCommand(cmd_dict)
     elif target == "Column":
-      column = self.columnFromIndex(cmd_dict["column_index"])
-      if (command == "Append") or (command == "Insert"):
-        name = cmd_dict["args"][0]
-        new_column = Column(name)
-        increment = 0
-        if command == "Append":
-          increment = 1
-        new_column_index = cmd_dict["column_index"] + increment
-        self.addColumn(new_column, new_column_index)
-      elif command == "Delete":
-        self.deleteColumn(column)
-      elif command == "Move":
-        try:
-          dest_column = self.columnFromName(cmd_dict["args"][0])
-          new_column_index = self.indexFromColumn(dest_column)
-          cur_column = self.columnFromIndex(cmd_dict["column_index"])
-          self.moveColumn(cur_column, new_column_index)
-        except Exception as e:
-          response = self._createResponse(success=False, message=str(e))
-          return response
-      elif command == "Rename":
-        column.rename(cmd_dict["args"][0])
-      else:
-        msg = "Unimplemented %s command: %s." % (target, command)
-        raise NotYetImplemented(msg)
+      error = self._columnCommand(cmd_dict)
     elif target == "Row":
-      row_index = cmd_dict['row_index']
-      if command == "Move":
-        new_name = cmd_dict["args"][0]
-        self.renameRow(row_index, new_name)
-      elif command == "Delete":
-        self.deleteRows([row_index])
-      elif command == "Insert":
-        row = self.getRow()
-        self.addRow(row, row_index - 0.1)  # Add a new row before 
-      elif command == "Append":
-        row = self.getRow()
-        self.addRow(row, row_index + 0.1)  # Add a new row after
-      else:
-        msg = "Unimplemented %s command: %s." % (target, command)
-        raise NotYetImplemented(msg)
+      error = self._rowCommand(cmd_dict)
+    elif target == "Table":
+      error = self._tableCommand(cmd_dict)
     else:
         msg = "Unimplemented %s." % target
         raise NotYetImplemented(msg)
-    response = self._createResponse(success=True)
+    response = self._createResponse(error)
     return response
+
+  def _tableCommand(self, cmd_dict):
+    # Processes a UI request for a Table
+    # Input: cmd_dict - dictionary with the keys
+    # Output: error from exception (may be none)
+    error = None
+    command = cmd_dict["command"]
+    msg = "Unimplemented Table command: %s." % command
+    raise NotYetImplemented(msg)
+
+  def _cellCommand(self, cmd_dict):
+    # Processes a UI request for a Cell
+    # Input: cmd_dict - dictionary with the keys
+    # Output: error from exception (may be none)
+    error = None
+    command = cmd_dict["command"]
+    if command == "Update":
+      self.updateCell(cmd_dict["value"], 
+                      cmd_dict["row_index"], 
+                      cmd_dict["column_index"])
+    else:
+      msg = "Unimplemented %s command: %s." % (target, command)
+      raise NotYetImplemented(msg)
+    return error
+
+  def _columnCommand(self, cmd_dict):
+    # Processes a UI request for a Column
+    # Input: cmd_dict - dictionary with the keys
+    # Output: error from exception (may be none)
+    error = None
+    command = cmd_dict["command"]
+    column = self.columnFromIndex(cmd_dict["column_index"])
+    if (command == "Append") or (command == "Insert"):
+      name = cmd_dict["args"][0]
+      new_column = Column(name)
+      increment = 0
+      if command == "Append":
+        increment = 1
+      new_column_index = cmd_dict["column_index"] + increment
+      self.addColumn(new_column, new_column_index)
+    elif command == "Delete":
+      self.deleteColumn(column)
+    elif command == "Move":
+      try:
+        dest_column = self.columnFromName(cmd_dict["args"][0])
+        new_column_index = self.indexFromColumn(dest_column)
+        cur_column = self.columnFromIndex(cmd_dict["column_index"])
+        self.moveColumn(cur_column, new_column_index)
+      except Exception as error:
+        pass
+    elif command == "Rename":
+      column.rename(cmd_dict["args"][0])
+    else:
+      msg = "Unimplemented %s command: %s." % (target, command)
+      raise NotYetImplemented(msg)
+    return error
+
+  def _rowCommand(self, cmd_dict):
+    # Processes a UI request for a Row
+    # Input: cmd_dict - dictionary with the keys
+    # Output: error from exception (may be none)
+    error = None
+    command = cmd_dict["command"]
+    row_index = cmd_dict['row_index']
+    if command == "Move":
+      new_name = cmd_dict["args"][0]
+      self.renameRow(row_index, new_name)
+    elif command == "Delete":
+      self.deleteRows([row_index])
+    elif command == "Insert":
+      row = self.getRow()
+      self.addRow(row, row_index - 0.1)  # Add a new row before 
+    elif command == "Append":
+      row = self.getRow()
+      self.addRow(row, row_index + 0.1)  # Add a new row after
+    else:
+      msg = "Unimplemented %s command: %s." % (target, command)
+      raise NotYetImplemented(msg)
+    return error
   
   def render(self, table_id="scitable"):
     # Input: table_id - how the table is identified in the HTML
