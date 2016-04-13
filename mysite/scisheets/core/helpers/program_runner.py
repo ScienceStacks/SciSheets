cf164a04a482cfeb9a68ff414f44c99408333477
Joseph Hellerstein <jlheller@uw.edu>
2016-04-12 18:07:02 -0700

    add forgotten files

---
+++ mysite/scisheets/core/helpers/program_runner.py
@@ -0,0 +1,97 @@
+"""
+Helper for program export and evaluation.
+"""
+
+
+class ProgramRunner(object):
+  """
+  Writes and/or runs one or more programs.
+  Assumes that the user_directory has the file my_api.py.
+  """
+
+  def __init__(self, program, user_directory, filename):
+    """
+    :param str program: string of one or more python program
+    :param str user_directory: user directory where program
+                               will execute. Must have file my_api.py
+    :param str filename: filename including extension
+    """
+    self._program = program
+    self._user_directory = user_directory
+    self._filepath = os.path.join(self._user_directory, filename)
+
+  def writeFile(self):
+    """
+    :return str error: error from file I/O
+    """
+    self._filepath = filepath
+    error = None
+    try:
+      with open(self._filepath, "w") as file_handle:
+        file_handle.write(self._program)
+    except IOError as err:
+      error = str(err)
+    return error
+
+  def _createAPIObject(self):  
+    """
+    Creates the API object needed for the runtime.
+    :return str error: error from execution
+    """
+    globals()['_table'] = self._table
+    program = """
+import myapi as api
+s = api.APIFormulas(_table)
+"""
+    return self._execute(program)
+
+  def _executeProgram(self, program):
+    """
+    :param str program: program to execute
+    :returns str: error from execution
+    Executes as a string if there is no filepath. Otherwise,
+    executes from the filepath.
+    """
+    error = None
+    # pylint: disable=W0122
+    try:
+      exec(self._program, globals())
+    # pylint: disable=W0703
+    except Exception as err:
+      # Report the error without changing the table
+      error = err
+    if error is not None:
+      # TODO: Better error message
+      #msg = "%s: %s" % (error.msg, error.text)
+      msg = str(error)
+    else:
+      msg = None
+    return msg
+
+  def execute(self, createAPIObject=False):
+    """
+    :param bool createAPIObject: True if the runner creates the API object
+    :returns str: error from execution
+    Executes as a string if there is no filepath. Otherwise,
+    executes from the filepath.
+    """
+    if not self._user_directory is None:
+      sys.path.append(self._user_directory)
+    error = None
+    if createAPIObject:
+      error = self._createAPIObject()
+    if error is None:
+      if self._filepath is not None:
+        # pylint: disable=W0122
+        try:
+          execfile(self._filepath, globals())
+        # pylint: disable=W0703
+        except Exception as err:
+          # Report the error without changing the table
+          error = err
+      elif len(self._program) > 0:
+        error = self._eecuteProgram(self._program)
+    if error is not None:
+      return str(error)
+    else:
+      return msg
---
+++ mysite/scisheets/core/helpers/test_program_generator.py
@@ -0,0 +1,68 @@
+'''Tests for program_generator'''
+
+import program_generator as pg
+from ..helpers_test import createTable,  \
+    stdoutIO, TableFileHelper, TEST_DIR
+import os
+import numpy as np
+import shutil
+import unittest
+
+
+# Constants
+COLUMN = "DUMMY"
+COLUMN1 = "DUMMY1"
+COLUMN2 = "A"
+COLUMN3 = "DUMMY3"
+COLUMN4 = "DUMMY4"
+COLUMN5 = "B"
+COLUMNC = "C"
+COLUMN_VALID_FORMULA = "VALID_FORMULA"
+COLUMN_INVALID_FORMULA = "INVALID_FORMULA"
+TABLE_NAME = "DUMMY_TABLE"
+LIST = [2.0, 3.0]
+LIST2 = [3.0]
+TABLE = 'DUMMY'
+VALID_FORMULA = "np.sin(A) + B"
+VALID_FORMULA_WITH_USER_FUNCTION = "np.sin(A) + timesTwo(B)"
+SECOND_VALID_FORMULA = "np.cos(C)"
+INVALID_FORMULA = "np.cun(A)" # Invalid function
+COLUMN1_CELLS = ["one", "two", "three"]
+COLUMN2_CELLS = [10.0, 20.0, 30.0]
+COLUMN5_CELLS = [100.0, 200.0, 300.0]
+COLUMNC_CELLS = [1000.0, 2000.0, 3000.0]
+IMPORT_PATHS = ["", "scisheets.core"]
+
+
+#############################
+# Tests
+#############################
+# pylint: disable=W0212
+# pylint: disable=C0111
+# pylint: disable=R0904
+class TestFunctions(unittest.TestCase):
+ 
+  def testMakeOutputStr(self):
+    result = pg._makeOutputStr(["a"])
+    self.assertEqual(result, "a")
+    result = pg._makeOutputStr(["a", "b"])
+    self.assertEqual(result, "a,b")
+    result = pg._makeOutputStr([])
+    self.assertEqual(result, "")
+
+
+class TestProgramGenerator(unittest.TestCase):
+
+  def setUp(self):
+    self.table = createTable(TABLE_NAME)
+    self._addColumn(COLUMN1, cells=COLUMN1_CELLS)
+    self.column_a = self._addColumn(COLUMN2, cells=COLUMN2_CELLS)
+    self.column_b = self._addColumn(COLUMN5, cells=COLUMN5_CELLS)
+    self.column_c = self._addColumn(COLUMNC, cells=COLUMNC_CELLS)
+    self.column_valid_formula = self._addColumn(COLUMN_VALID_FORMULA,
+                                                formula=VALID_FORMULA)
+    self.pgm_gen = pg.ProgramGenerator(self.table, TEST_DIR)
+
+
+if __name__ == '__main__':
+  unittest.main()
