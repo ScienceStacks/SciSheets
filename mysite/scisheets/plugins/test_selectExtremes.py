8ebce82c0b56cd724b3eb767aa24edb440a2b3ee
Joseph Hellerstein <jlheller@uw.edu>
2016-05-11 17:11:42 -0700

    Added plugins for selecting extreme values

---
+++ mysite/scisheets/plugins/test_selectExtremes.py
@@ -0,0 +1,29 @@
+""" Tests for selectExtremes """
+
+from selectExtremes import selectExtremes
+import unittest
+import numpy as np
+
+RANGE = range(4)
+DOUBLE_RANGE = range(2*len(RANGE))
+
+
+class TestSelectExtremes(unittest.TestCase):
+
+  def testSimple(self):
+    fltr = selectExtremes(RANGE, 1)
+    self.assertTrue(fltr[0])
+    fltr = selectExtremes(RANGE, 2)
+    self.assertFalse(all(fltr))
+
+  def testMore(self):
+    values = [1, 3, -1, 2]
+    fltr = selectExtremes(values, 1)
+    self.assertTrue(fltr[2])
+    fltr = selectExtremes(values, 0.5)
+    self.assertTrue(fltr == [True, False, True, False])
+
+
+if __name__ == '__main__':
+  unittest.main()
+
