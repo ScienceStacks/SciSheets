be957b4645795a5401d7b1ca6d92bfacf48be349
Joseph Hellerstein <jlheller@uw.edu>
2016-05-13 10:43:04 -0700

    Completed tabularize implementation.

--- mysite/scisheets/plugins/tabularize.py
+++ mysite/scisheets/plugins/tabularize.py
@@ -18,7 +18,7 @@ CATEGORICAL as follows:
 """
 
 import collections
-import pandas as pd
+import numpy as np
 
 def _delElement(an_iterable, idx):
   """
@@ -31,6 +31,16 @@ def _delElement(an_iterable, idx):
   back_list = a_list[(idx+1):]
   new_list.extend(back_list)
   return new_list
+
+def _uniqueElements(an_iterable):
+  """
+  :param iterable an_iterable:
+  :param int idx:
+  :return list: has only one occurrence of each element
+  """
+  used = []
+  unique = [x for x in an_iterable if x not in used and (used.append(x) or True)]
+  return unique
   
 
 def tabularize(s, 
@@ -38,10 +48,11 @@ def tabularize(s,
                category_index,
                values_colnm, 
                new_category_colnm=None,
-               values_colnm_prefix="New"):
+               values_colnm_prefix="Col"):
   """
   :param APIObject s:
-  :param str category_colnm: name of the category column
+  :param str category_colnm: name of the category column with cells that
+                             have a list of category values
   :param int category_index: index of the list elements in the category column
                              to use in defining the new values columns
   :param str values_colnm: name of the column containing values
@@ -49,35 +60,41 @@ def tabularize(s,
                                   is to prepend "New" to the existing name
   :param str values_colnm_prefix: Prefix that prepends the new values column
   """
-####### FIX SO CATEGORY COLUMN ALIGNS WITH VALUES:w
-
+  # Initializations
   if new_category_colnm is None:
     new_category_colnm = "New%s" % category_colnm
-  category_elements = s.getColumnValues(category_colnm)
-  values = s.getColumnValues(values_colnm)
+  category_elements = [ele for ele in s.getColumnValues(category_colnm)  \
+                       if ele is not None]
+  values = []
+  for val in s.getColumnValues(values_colnm):
+    if isinstance(val, float):
+      if not np.isnan(val):
+        values.append(val)
+    else:
+      if not val is None:
+        values.append(val)
   if len(category_elements) != len(values):
     raise ValueError('Unequal lengths for cateogry column %s and values column %s' %  \
         (category_colnm, values_colnm))
-  new_category_elements = []
-  category_suffixes = set([ele[category_index] for ele in category_elements])
-  new_frame = {}
-  new_category = set([_delElement(ele, category_index) for ele in category_elements])
-  for cat in category_suffixes:
-    new_frame[cat] = [np.nan for n in len(values)]
-  new_frame[new_category_colnm] = []
-  for new_cat in new_category:
-    new_frame[new_category_colnm].append(new_cat)
+  size = len(values)
+  # Construct the column values
+  col_dict = {}
+  elements = [_delElement(ele, category_index) for ele in category_elements]
+  col_dict[new_category_colnm] = _uniqueElements(elements)
+  new_length = len(col_dict[new_category_colnm])
+  colnm_suffixes = set([ele[category_index] for ele in category_elements])
+  for sfx in colnm_suffixes:
+    col_dict[sfx] = [np.nan for n in range(new_length)]
   for idx in range(len(values)):
-    full_cat = category_elements[idx]
-    colnm_cat = full_cat[idx]
-    new_cat = _delElement(full_cat, category_index)
-    new_frame[cat].append(values[idx])
-  for cat in category_suffixes:
-    name = "%s%s" % (values_colnm_prefix, str(cat))
+    cur_cat = category_elements[idx]
+    sfx = cur_cat[category_index]
+    new_cat = _delElement(cur_cat, category_index)
+    row_idx = col_dict[new_category_colnm].index(new_cat)
+    col_dict[sfx][row_idx] = values[idx]
+  for sfx in colnm_suffixes:
+    name = "%s%s" % (values_colnm_prefix, str(sfx))
     s.createColumn(name)
-    s.setColumnValues(name, new_values[cat])
+    s.setColumnValues(name, col_dict[sfx])
   s.createColumn(new_category_colnm)
-  s.setColumnValues(new_category_colnm, new_elements)
-  
-
-  
+  s.setColumnValues(new_category_colnm, 
+                    col_dict[new_category_colnm])
--- mysite/scisheets/plugins/test_tabularize.py
+++ mysite/scisheets/plugins/test_tabularize.py
@@ -2,6 +2,7 @@
 
 from scisheets.core.api import APIFormulas
 from scisheets.core.table import Table
+from scisheets.core.helpers.cell_types import isNan
 from tabularize import tabularize, _delElement
 import unittest
 import pandas as pd
@@ -9,13 +10,20 @@ import pandas as pd
 
 CATEGORY_COLNM = 'category'
 VALUES_COLNM = 'values'
+SFX_NAMES = ['a', 'b']
+OTHER_NAMES = ['x', 'y']
+VALUES = range(4)
 
 
 class TestTabularize(unittest.TestCase):
 
   def setUp(self):
-    val_dict = {CATEGORY_COLNM: [ ['x', 'a'], ['x', 'b'], ['y', 'a'], ['y', 'b']],
-        VALUES_COLNM:  range(4)
+    cat_values = []
+    for o in OTHER_NAMES:
+      for s in SFX_NAMES:
+        cat_values.append([o, s])
+    val_dict = {CATEGORY_COLNM: cat_values,
+        VALUES_COLNM:  VALUES,
         }
     df = pd.DataFrame(val_dict)
     self.api = APIFormulas(Table("Dummy"))
@@ -30,8 +38,20 @@ class TestTabularize(unittest.TestCase):
       self.assertTrue(expected_list == _delElement(values, idx))
 
   def testSimple(self):
-    tabularize(self.api, CATEGORY_COLNM, 1, VALUES_COLNM)
-    import pdb; pdb.set_trace()
+    new_category_colnm = "NewCategory"
+    values_colnm_prefix = "Col"
+    tabularize(self.api, CATEGORY_COLNM, 1, VALUES_COLNM,
+        new_category_colnm=new_category_colnm,
+        values_colnm_prefix=values_colnm_prefix)
+    table = self.api._table
+    self.assertTrue(table.isColumnPresent(new_category_colnm))
+    for sfx in SFX_NAMES:
+      expected_name = "%s%s" % (values_colnm_prefix, sfx)
+      self.assertTrue(table.isColumnPresent(expected_name))
+      column = table.columnFromName(expected_name)
+      cells = [x for x in column.getCells() if not isNan(x)]
+      size = len(VALUES)/len(SFX_NAMES)
+      self.assertEqual(len(cells), size)
 
 
 if __name__ == '__main__':
