7456b152048787a6a053ba898e0dac0f7107164c
Joseph Hellerstein <jlheller@uw.edu>
2017-02-03 09:57:12 -0800

    Remove saved code.

--- mysite/scisheets/ui/save/test_html_column.py
+++
@@ -1,8 +0,0 @@
-'''Tests for html_column.'''
-
-from mysite import settings
-from mysite.scisheets.ui import HTMLTable
-from django.test import TestCase  # Provides mocks for client interactions
-
-class TestHTMLColumn(TestCase):
-  pass
