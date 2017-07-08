4b3c32d3cc644411264bff8fe89f38e6611fe284
Alicia Clark <clarka34@uw.edu>
2017-07-07 22:47:57 -0700

    finding path to root of repo.

--- bin/get_basedir.sh
+++ bin/get_basedir.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-SOURCE="${BASH_SOURCE[0]}"
-BASEDIR=$(dirname $(dirname "$SOURCE"))
+SOURCE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >>/dev/null && pwd )"
+BASEDIR=$(dirname "$SOURCE")
 echo $BASEDIR
