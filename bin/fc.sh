4ad6c00b067e6101535fb437118faf177f33469c
Joseph Hellerstein <jlheller@uw.edu>
2017-07-12 11:43:24 -0700

    Useful script

---
+++ bin/fc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Finds the files that need to be changed
+LIST="CodeGeneration Files Testing Tree CommonUtil FileVersion Database"
+clear
+for x in $LIST
+do
+  echo " "
+  echo "*** $x ***"
+  ft.sh $x *.py | grep -v "Binary"
+done
